c306be376f6c3943682cf59f94433424a272d5d8
Debbie <asamoahjaeldeborah@gmail.com>
2022-11-22 18:35:20 +0000

    Initial commit

---
+++ src/components/widget/index.jsx
@@ -0,0 +1,108 @@
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
+
+import "./widget.scss"
+
+const Widget = ({ type }) => {
+  let data;
+
+  //temporary
+  const amount = 100;
+  const diff = 20;
+
+    switch (type) {
+        case "user":
+            data = {
+                title: "USERS",
+                isMoney: false,
+                link: "See all users",
+                icon: (
+                    <PersonOutlinedIcon
+                        className="icon text-[18px] p-[5px] rounded-[5px] self-end "
+                        style={{
+                            color: "crimson",
+                            backgroundColor: "rgba(255, 0, 0, 0.2)",
+                        }}
+                    />
+                )
+            }
+        break;
+    
+        case "order":
+            data = {
+                title: "ORDERS",
+                isMoney: false,
+                link: "View all orders",
+                icon: (
+                    <ShoppingCartOutlinedIcon
+                        className="icon text-[18px] p-[5px] rounded-[5px] self-end "
+                        style={{
+                            backgroundColor: "rgba(218, 165, 32, 0.2)",
+                            color: "goldenrod",
+                            }}
+                    />
+                )
+            };
+        break;
+        
+        case "earning":
+            data = {
+                title: "EARNINGS",
+                isMoney: true,
+                link: "View net earnings",
+                icon: (
+                    <MonetizationOnOutlinedIcon
+                        className="icon text-[18px] p-[5px] rounded-[5px] self-end "
+                        style={{ backgroundColor: "rgba(0, 128, 0, 0.2)", color: "green" }}
+                    />
+                )
+            }      
+        break;
+        
+        case "balance":
+            data = {
+                title: "BALANCE",
+                isMoney: true,
+                link: "See details",
+                icon: (
+                    <AccountBalanceWalletOutlinedIcon
+                        className="icon text-[18px] p-[5px] rounded-[5px] self-end "
+                        style={{
+                            backgroundColor: "rgba(128, 0, 128, 0.2)",
+                            color: "purple",
+                        }}
+                    />
+                ),
+            }
+        break;
+        
+        default:
+        break;
+    }
+
+
+    return (
+        <div className="flex justify-between p-[10px] shadow-xl -webkit-shadow-xl rounded-[10px] h-[100%]   ">
+            <div className="flex flex-col justify-between  ">
+                <span className="text-[14px] font-bold text-[rgb(160, 160, 160)] ">{data.title}</span>
+                <span className="text-[28px] font-bold ">
+                    {data.isMoney && "$"} {amount}
+                </span>
+                <span className="w-[100%] text-[12px] border-[1px] border-b-gray border-x-transparent border-t-transparent ">{data.link}</span>
+            </div >
+      
+            <div className="flex flex-col justify-between ">
+                <div className="percentage positive">
+                    <KeyboardArrowUpIcon />
+                    {diff} %
+                </div>
+                {data.icon}
+            </div>
+        </div>
+    )
+}
+
+export default Widget;
